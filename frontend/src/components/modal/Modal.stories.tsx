ded614bc44c673ef2f4daf1791e86dae5336c2c3
jimmyNicholas <jimmynicholas@duck.com>
2025-04-13 11:05:45 +1000

    Added default modal story

---
+++ frontend/src/components/modal/Modal.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Modal } from './Modal';
+
+const meta = {
+  component: Modal,
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Modal Button',
+    buttonType: 'success',
+    buttonStyle: '',
+    ModalBoxChild: <div>Modal Box</div>,
+    modalId: "modalId"
+  }
+};
